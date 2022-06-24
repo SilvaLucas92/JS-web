a1c1c79b5e7158d262cd7b140abaf27d92db4aad
Lucas R Silva <Lucas.Silva@gds.ey.com>
2022-06-24 15:59:22 -0300

    about me fixed

--- src/components/AboutMe/About.js
+++ src/components/AboutMe/About.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {   
     Flex,
     Heading,
     Text,
     Box,
     Image,
-    Center
+    Center,
+    HStack,
+    IconButton
     } from '@chakra-ui/react';
-import terapia from './terapia.jpg';
-import ya from './1.jpg'
+import ya from './1.jpg';
+import data from '../AboutMe/data';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+
 const About = () => {
+
+  const [ index, setIndex ] = useState(0);
+  const { title, text } = data[index];
+
   return (
     <Center
     as='section'
@@ -17,40 +25,31 @@ const About = () => {
     w= '100%'
     id='about-me'
     >
-      <Flex 
+        <Flex 
         w={{base: '90%', md: '80%'}}
         maxW= '1000px'
         mx='auto'
-        // mt='100px'
-        // mb='50px'
         my='100px'
         align= 'center'
         justify= 'center'
         direction={{base:'column', md:'row'}}
-    //   data-aos="fade-up"
-      >
-        {/* <Box
-        w={{base:'90%', sm:'80%' ,md:'70%', xl:'55%'}}
-        h={{base:'400px', md:'600px'}}
-        > */}
+        //   data-aos="fade-up"
+        >
         <Image 
         src= {ya}
-        // h='100%'
-        // w='100%'
-        w={{base:'90%', sm:'80%' ,md:'70%', xl:'55%'}}
+        w={{base:'90%', sm:'80%' ,md:'60%', xl:'55%'}}
         minH={{base:'400px', md:'600px'}}
         borderRadius= '8px'
                 />       
-        {/* </Box> */}
-        <Box
-        w={{base:'80%',sm:'70%' ,md:'50%'}}
-        p={{base:'15px', xl:'20px'}}
-        bg= '#ffffff'
-        borderRadius= '8px'
-        ml= {{base: '0px', md:'-100px'}}
-        mt={{base:'-80px', md:'0px'}}
-        textAlign='center'
-        >
+            <Box
+            w={{base:'80%',sm:'70%' ,md:'50%'}}
+            p={{base:'15px', xl:'20px'}}
+            bg= '#ffffff'
+            borderRadius= '8px'
+            ml= {{base: '0px', md:'-100px'}}
+            mt={{base:'-80px', md:'0px'}}
+            textAlign='center'
+            >
                 <Heading
                 as='h2'
                 color='#2d3748' 
@@ -64,21 +63,37 @@ const About = () => {
                 }}
                 fontWeight='bold'
                 >
-                   Conoceme 
+                    { title } 
                 <Box borderTop='2px' color='rgb(49 151 149)' w='25%' mx='auto' mt={2} />
                 </Heading>
 
-              <Text
+                <Text
                 mt={5}
                 opacity='0.7'
                 fontSize={{base: '15x', xl:'18px'}}
-              >
-                Psicologo egresado de la UBA - especializacion en psicoterapia sistemica. Movilizado por deseo de acompañar a las personas a lograr una mejor calidad de vida, atiendo en centros de salud integrales y en mi consultorio privado. me gusta leer, escribir y el cine. Convencido de que siempre en el problema se encuentra la solucion.
-                Psicólogo egresado en la Universidad de Buenos Aires (UBA) – Especialización en Psicoterapia Sistémica. 
-                Trabajo en APS (Atención Primaria de Salud) en Centros de Salud Integral, formando parte de equipos interdisciplinarios con Médicos familiares, Pediatras, Cardiólogos, Enfermeras, Ginecólogas, Obstetras, Trabajadoras Sociales y Psiquiatras. 
-              </Text>    
-              </Box>        
-      </Flex>
+                >
+                    { text }
+                </Text>
+                <HStack pt={5} justify='center'>
+                <IconButton 
+                isRound='true' 
+                onClick={() => index === 0? setIndex(data.length - 1) : setIndex(index - 1)} 
+                icon={<AiOutlineArrowLeft />} 
+                backgroundColor='#ffffff'
+                colorScheme='teal' 
+                variant='outline'
+                />
+                <IconButton 
+                isRound='true' 
+                onClick={() => index === (data.length - 1)  ? setIndex(0) : setIndex(index + 1)} 
+                icon={<AiOutlineArrowRight />} 
+                backgroundColor='#ffffff'
+                colorScheme='teal' 
+                variant='outline'  
+                />
+                </HStack>    
+            </Box>        
+        </Flex>
     </Center>
   )
 }
---
+++ src/components/AboutMe/data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        title: 'About me',
+        text: 'Psicologo egresado de la UBA - especializacion en psicoterapia sistemica. Movilizado por deseo de acompañar a las personas a lograr una mejor calidad de vida, atiendo en centros de salud integrales y en mi consultorio privado. me gusta leer, escribir y el cine. Convencido de que siempre en el problema se encuentra la solucion. Psicólogo egresado en la Universidad de Buenos Aires (UBA) – Especialización en Psicoterapia Sistémica. Trabajo en APS (Atención Primaria de Salud) en Centros de Salud Integral, formando parte de equipos interdisciplinarios con Médicos familiares, '
+    },
+    {
+        id: 2,
+        title: 'Experiencia Laboral',
+        text: 'Psicologo egresado de la UBA - especializacion en psicoterapia sistemica. Movilizado por deseo de acompañar a las personas a lograr una mejor calidad de vida, atiendo en centros de salud integrales y en mi consultorio privado. me gusta leer, escribir y el cine. Convencido de que siempre en el problema se encuentra la solucion. Psicólogo egresado en la Universidad de Buenos Aires (UBA) – Especialización en Psicoterapia Sistémica. Trabajo en APS (Atención Primaria de Salud) en Centros de Salud Integral, formando parte de equipos interdisciplinarios con Médicos familiares,'
+    },
+    {
+        id: 3,
+        title: 'Articulos',
+        text: 'Psicologo egresado de la UBA - especializacion en psicoterapia sistemica. Movilizado por deseo de acompañar a las personas a lograr una mejor calidad de vida, atiendo en centros de salud integrales y en mi consultorio privado. me gusta leer, escribir y el cine. Convencido de que siempre en el problema se encuentra la solucion. Psicólogo egresado en la Universidad de Buenos Aires (UBA) – Especialización en Psicoterapia Sistémica. Trabajo en APS (Atención Primaria de Salud) en Centros de Salud Integral, formando parte de equipos interdisciplinarios con Médicos familiares, '
+    }
+]
