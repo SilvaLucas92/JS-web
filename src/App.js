d1c055e51a705f89913cf5a7dfb07ec307d05c81
Lucas R Silva <Lucas.Silva@gds.ey.com>
2022-06-30 17:11:55 -0300

    animations

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Servicios from './components/Servicios/Servicios';
 import FrequentQuestions from './components/FrequentQuestions/FrequentQuestions'
+import Head from './components/Header/Head';
+import HeroPr from './components/Hero/HeroPr';
 function App() {
   return (
     <>
-      <Header />,
-      <Hero />
+      <Head />
+      {/* <Header />, */}
+      {/* <Hero /> */}
+      <HeroPr />
       {/* <About /> */}
       <AboutMe />
       <Servicios />
--- src/components/Header/Head.js
+++ src/components/Header/Head.js
@@ -1,77 +1,189 @@
-import React from "react";
-import {
-  Box,
-  Stack,
-  Heading,
-  Flex,
-  Text,
-  Button,
-  useDisclosure
-} from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { Button, Flex, Heading, HStack,  IconButton, Stack, chakra } from '@chakra-ui/react';
+import { AiOutlineAlignRight,  AiOutlineClose} from 'react-icons/ai';
+import { Link } from 'react-scroll';
+import React, { useState, useEffect } from 'react'
+import { motion, isValidMotionProp } from 'framer-motion';
 
-// Note: This code could be better,
-// so I'd recommend you to understand how I solved and you could write yours better :)
-// Good luck! 🍀
+const ChakraBox = chakra(motion.header, {
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
+});
 
-// Update: Check these awesome headers from Choc UI 👇
-// https://choc-ui.tech/docs/elements/headers
-const Head = (props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const handleToggle = () => (isOpen ? onClose() : onOpen());
+const Head = () => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') { 
+      if (window.scrollY > lastScrollY) {
+        setShow(false); 
+      } else {
+        setShow(true);  
+      }
+      setLastScrollY(window.scrollY); 
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding={6}
-      bg="teal.500"
-      color="white"
-      position="sticky"
-      left='0'
-      right='0'
-      top='0'
-      zIndex= '99'
-      {...props}
+    <ChakraBox
+    w='100%'
+    top={show ? 0 : '-100px'}
+    left='0'
+    right='0'
+    bg='#ffffff'
+    borderBottomWidth={1}
+    position='fixed'
+    zIndex='1'
+    boxShadow='xl'
+    initial={{y:-250}}
+    animate={{y:0}}
+    transition={{delay:0.5, type: "tween"}}
     >
-      <Flex align="center" mr={5}>
-        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
-          Chakra UI
-        </Heading>
+      <Flex >
+        <HStack
+          w={{base: '90%', md: '80%'}}
+          mx='auto'
+          justify='space-between'
+          align='center'
+          p={5}>
+          <Heading 
+          display={{base: 'none', xl: 'block'}}
+          fontWeight="bold"
+          color="#2b2c34"
+          >
+              Julio Silva
+          </Heading>
+          <Heading  display={{base: 'block', xl:'none'}}>
+              JS
+          </Heading>
+          <Flex
+            display={{md:'none'}}
+          >
+            <IconButton
+              icon={!showMenu? <AiOutlineAlignRight /> : <AiOutlineClose />} 
+              onClick={() => {return !showMenu? setShowMenu(true) : setShowMenu(false)}}
+              fontSize={22}
+              isRound='true'
+              backgroundColor='#ffffff'
+              colorScheme='teal' 
+              variant='outline'
+            />
+          </Flex>
+
+          <Flex
+            as='nav'
+            display={{base:'none', md:"flex"}}
+            justify='center'
+            align='center'
+          >
+            <Button m={2} variant='link'>
+            <Link
+              to='about-me'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+            >
+            About Me</Link>
+            </Button>
+            <Button m={2} variant='link'>
+            <Link
+              to='services'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+            >
+            Nuestros servicios</Link>
+            </Button>
+            <Button m={2} variant='link'>
+            <Link
+              to='frequentQuestions'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+            >
+            Preguntas Frecuentes</Link>
+            </Button>
+            <Button m={2} variant='link'>
+            <Link
+              to='contact-me'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+            >
+            Contacto</Link>
+            </Button>
+          </Flex>  
+        </HStack>
       </Flex>
-
-      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
-        <HamburgerIcon />
-      </Box>
-
-      <Stack
-        direction={{ base: "column", md: "row" }}
-        display={{ base: isOpen ? "block" : "none", md: "flex" }}
-        width={{ base: "full", md: "auto" }}
-        alignItems="center"
-        flexGrow={1}
-        mt={{ base: 4, md: 0 }}
-      >
-        <Text>Docs</Text>
-        <Text>Examples</Text>
-        <Text>Blog</Text>
-      </Stack>
-
-      <Box
-        display={{ base: isOpen ? "block" : "none", md: "block" }}
-        mt={{ base: 4, md: 0 }}
-      >
-        <Button
-          variant="outline"
-          _hover={{ bg: "teal.700", borderColor: "teal.700" }}
-        >
-          Create account
-        </Button>
-      </Box>
-    </Flex>
-  );
+      <Flex
+          as='nav'
+          display={showMenu? 'flex' : 'none'}
+          direction='column'
+          w='100%'
+          mx='auto'
+          align='center'
+          bg='#ffffff'
+          >
+            <Button my={8} variant='link' size='lg'>
+            <Link
+              to='about-me'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+              onClick={() => {return setShowMenu(false)}}
+            >
+            About Me</Link>
+            </Button>
+            <Button my={8} variant='link' size='lg'>
+            <Link
+              to='services'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+              onClick={() => {return setShowMenu(false)}}
+            >
+            Nuestros servicios</Link>
+            </Button>
+            <Button my={8} variant='link' size='lg'>
+            <Link
+              to='frequentQuestions'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+              onClick={() => {return setShowMenu(false)}}
+            >
+            Preguntas frecuentes</Link>
+            </Button>
+            <Button my={8} variant='link' size='lg'>
+            <Link
+              to='contact-me'
+              spy={true} 
+              smooth={true} 
+              offset={100} 
+              duration={800}
+              onClick={() => {return setShowMenu(false)}}
+            >
+            Contacto</Link>
+            </Button>
+        </Flex> 
+    </ChakraBox> 
+  )
 }
 
-export default Head
+export default Head;
