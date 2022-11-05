bb5bf25e1cd933ae56df0d8770fd3a8ee5c04ff6
SilvaLucas92 <l.silva8692@gmail.com>
2022-11-05 19:58:22 -0300

    Change Contact

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,21 +1,109 @@
-import { Text } from "@chakra-ui/react";
-import SingleContact from "./SingleContact/SingleContact";
+import {
+  Stack,
+  Flex,
+  SimpleGrid,
+  Text,
+  Icon,
+  IconButton,
+  Button,
+  Tooltip,
+  useClipboard,
+} from "@chakra-ui/react";
+import { FcFinePrint, FcCollaboration, FcBinoculars } from "react-icons/fc";
+import { BsWhatsapp } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
 import Section from "../Section/Section";
+const Feature = ({ icon }) => {
+  return (
+    <Stack align="center">
+      <Flex
+        w={16}
+        h={16}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        mb={1}
+      >
+        {icon}
+      </Flex>
+    </Stack>
+  );
+};
 
 const Contacto = () => {
+  const { hasCopied, onCopy } = useClipboard("example@example.com");
   return (
-    <Section id="contact-me" title="Contacto">
+    <Section id="contact-me" title="Contacto" backgroundColor="#f7fafc">
       <Text
         textAlign="center"
-        color="#ffffff"
-        p={5}
         fontSize={{ base: "18px", md: "20px" }}
         opacity="0.8"
+        mb={10}
       >
         Si tenes alguna duda y queres escribirnos, te dejamo tos los medio para
         que te comuniques
       </Text>
-      <SingleContact />
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+        <Feature
+          icon={
+            <Tooltip label={"Send a messagge"} closeOnClick={false} hasArrow>
+              <IconButton
+                aria-label="whatsapp"
+                href="https://wa.link/9fq5vs"
+                variant="ghost"
+                size="lg"
+                fontSize="4xl"
+                icon={<BsWhatsapp />}
+                _hover={{
+                  bg:"#4FD1C5"
+                }}
+                isRound
+              />
+            </Tooltip>
+          }
+        />
+        <Feature
+          icon={
+            <Tooltip
+              label={hasCopied ? "Email Copied!" : "Copy Email"}
+              closeOnClick={false}
+              hasArrow
+            >
+              <IconButton
+                aria-label="email"
+                variant="ghost"
+                size="lg"
+                fontSize="4xl"
+                icon={<AiOutlineMail />}
+                _hover={{
+                  bg:"#4FD1C5"
+                }}
+                onClick={onCopy}
+                isRound
+              />
+            </Tooltip>
+          }
+        />
+        <Feature
+          icon={
+            <Tooltip label={"See where am I!"} closeOnClick={false} hasArrow>
+              <IconButton
+                aria-label="whatsapp"
+                href="https://wa.link/9fq5vs"
+                variant="ghost"
+                size="lg"
+                fontSize="4xl"
+                icon={<GoLocation />}
+                _hover={{
+                  bg:"#4FD1C5"
+                }}
+                isRound
+              />
+            </Tooltip>
+          }
+        />
+      </SimpleGrid>
     </Section>
   );
 };
